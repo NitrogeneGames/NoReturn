4971e083470703c462e4b57a8f7140fd622cd287
A. K. <arpad.kovesdy@gmail.com>
2013-09-15 20:26:52 -0500

    Map planet rand started

---
+++ src/nitrogene/world/arenamap.java
@@ -0,0 +1,30 @@
+package nitrogene.world;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import nitrogene.core.Planet;
+
+public class arenamap {
+	private int planetnumber;
+	private ArrayList<Planet> planetlist;
+	Random random;
+	
+	public arenamap(int planetnumber){
+		this.planetnumber = planetnumber;
+		planetlist = new ArrayList<Planet>();
+		generate();
+	}
+	
+	private void generate(){
+		
+	}
+	
+	private void addPlanet(){
+		random.
+	}
+	
+	public ArrayList<Planet> getPlanets(){
+		return planetlist;
+	}
+}
