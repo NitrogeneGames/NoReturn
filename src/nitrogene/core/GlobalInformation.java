f2aee657d638b6aec490095fd09074434ece139d
Arpad <Arpad@10.20.2.40>
2014-05-06 12:04:52 -0500

    Added GUI to ShipState (needs alignment) and created Game-Breaking bug in Debug mode

--- src/nitrogene/core/GlobalInformation.java
+++ src/nitrogene/core/GlobalInformation.java
@@ -1,16 +1,40 @@
 package nitrogene.core;
 
+import java.awt.Color;
+import java.awt.FontFormatException;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.newdawn.slick.SlickException;
 
 import nitrogene.weapon.EnumWeapon;
 
 public class GlobalInformation {
-		
+	
 	private static ArrayList<CraftData> craftdata = new ArrayList<CraftData>();
 	public static int selected;
 	public static boolean shipsLoaded = true;
 	public GlobalInformation(){
 		
+	}
+	
+	public org.newdawn.slick.UnicodeFont getFont(Float size) throws SlickException{
+		try {
+			java.awt.Font mainFont = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT,org.newdawn.slick.util.ResourceLoader.getResourceAsStream("fonts/acknowtt.ttf"));
+			mainFont = mainFont.deriveFont(java.awt.Font.PLAIN, size);
+			org.newdawn.slick.UnicodeFont uniFont = new org.newdawn.slick.UnicodeFont(mainFont);
+			uniFont.addAsciiGlyphs();
+	        org.newdawn.slick.font.effects.ColorEffect a = new org.newdawn.slick.font.effects.ColorEffect();
+	        a.setColor(Color.white);
+	        uniFont.getEffects().add(a);
+	        uniFont.loadGlyphs();
+	        return uniFont;
+		} catch (FontFormatException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return null;
 	}
 	
 	public static void setCraftDataOverride(ArrayList<CraftData> start){
--- src/nitrogene/core/ShipState.java
+++ src/nitrogene/core/ShipState.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import nitrogene.util.Button;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -18,8 +19,12 @@ import org.newdawn.slick.state.StateBasedGame;
 public class ShipState extends BasicGameState{
 
 	private int width,height;
+	private int scalefactor, obserx, obsery;
 	private Image backgroundimg;
+	private Color backgroundcolor;
 	private ArrayList<Button> buttonList = new ArrayList<Button>();
+	
+	private Button startButton, hangarButton, menuButton, minusButton, plusButton;
 	
 	public ShipState(int width,int height){
 		this.width = width;
@@ -28,9 +33,31 @@ public class ShipState extends BasicGameState{
 	
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		backgroundimg = new Image("res/hangar/background.png");
+		backgroundimg = new Image("res/hangar/shipselectionfinal.png");
+		backgroundimg.setFilter(Image.FILTER_NEAREST);
+		backgroundcolor = new Color(91, 91, 91);
+		
+		this.scalefactor = (int) Math.floor(height/128);
+		obserx = (width/2)-(backgroundimg.getWidth()*scalefactor/2);
+		obsery = (height/2)-(backgroundimg.getHeight()*scalefactor/2);
+		
 		//button init
-
+		Image startButtonImage = new Image("res/hangar/startbuttonhangar.png");
+		Image hangarButtonImage = new Image("res/hangar/startbuttonhangar.png");
+		Image menuButtonImage = new Image("res/hangar/startbuttonhangar.png");
+		Image minusButtonImage = new Image("res/hangar/minusbutton.png");
+		Image plusButtonImage = new Image("res/hangar/plusbutton.png");
+		
+		try {
+			startButton = new Button("Start", obserx+(125*scalefactor), obsery+(106*scalefactor), 15*scalefactor, 7*scalefactor, startButtonImage, null, null, null);
+			hangarButton = new Button("Design", obserx+(27*scalefactor), obsery+(106*scalefactor), 15*scalefactor, 7*scalefactor, hangarButtonImage, null, null, null);
+			menuButton = new Button("Menu", obserx+(10*scalefactor), obsery+(106*scalefactor), 15*scalefactor, 7*scalefactor, menuButtonImage, null, null, null);
+			minusButton = new Button("", obserx+(80*scalefactor), obsery+(106*scalefactor), 10*scalefactor, 9*scalefactor, minusButtonImage, null, null, null);
+			plusButton = new Button("", obserx+(91*scalefactor), obsery+(106*scalefactor), 10*scalefactor, 9*scalefactor, plusButtonImage, null, null, null);
+		} catch (FontFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@Override
@@ -57,6 +84,12 @@ public class ShipState extends BasicGameState{
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
+		startButton.update(container);
+		hangarButton.update(container);
+		menuButton.update(container);
+		minusButton.update(container);
+		plusButton.update(container);
+		
 		for(int i = 0; i < buttonList.size(); i++) {
 			buttonList.get(i).update(container);
 			if(buttonList.get(i).isClicked()){
@@ -70,9 +103,17 @@ public class ShipState extends BasicGameState{
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
+		g.setBackground(backgroundcolor);
+		backgroundimg.draw(obserx, obsery, scalefactor);
 		for(int i = 0; i < buttonList.size(); i++) {
 			buttonList.get(i).render(g);
 		}
+		
+		startButton.render(g);
+		hangarButton.render(g);
+		menuButton.render(g);
+		minusButton.render(g);
+		plusButton.render(g);
 	}
 
 	public int getID() {
